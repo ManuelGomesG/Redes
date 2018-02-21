0d9969cee0df4d366c41026d1037e040f6ac7041
manuel <manuelgomesg@gmail.com>
2018-02-21 17:31:00 -0400

    error primer caracter

--- cliente.java
+++ cliente.java
@@ -1,5 +1,6 @@
 import java.net.*;
 import java.io.*;
+import java.util.*;
 
 
 
@@ -11,13 +12,18 @@ public class cliente {
   public static void main(String[] args) {
     final String HOST = "localhost";
     final int PUERTO=5000;
+    DataOutputStream comando;
 
     Socket conexion;
 
     try {
       conexion = new Socket(HOST,PUERTO);
+      Scanner sc = new Scanner(System.in);
+      comando = new DataOutputStream(conexion.getOutputStream());
+      comando.writeUTF(sc.nextLine());
     }catch(Exception e){
       System.out.println("cancer" +e.getMessage());
+
     }
 
 
--- servidor.java
+++ servidor.java
@@ -19,6 +19,7 @@ public class servidor {
     Socket conexion;
 
     DataOutputStream salida;
+    BufferedReader entrada;
     BufferedOutputStream buffsalida;
     BufferedInputStream buffentrada;
     String recibido;
@@ -32,8 +33,10 @@ public class servidor {
         System.out.println("Esperando conexion");
         conexion=servidor.accept();
         System.out.println("Conexion establecida");
+        entrada=new BufferedReader(new InputStreamReader(conexion.getInputStream()));
+        System.out.println(entrada.readLine());
 
-        
+
       }
 
     }catch(Exception e)
