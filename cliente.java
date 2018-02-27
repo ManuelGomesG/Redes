55761f0dc99cde345a3e0f3555464fbfa12f239c
josedavidb <josedavidb6@gmail.com>
2018-02-26 20:52:13 -0400

    Transferencia de archivo. Falta hacerlo interactivo y solucionar problema de pase de mensajes

--- cliente.java
+++ cliente.java
@@ -17,26 +17,56 @@ public class cliente {
     BufferedReader entrada;
     String respuesta;
     String line;
+    String linea;
 
 
     Socket conexion;
 
     try {
       conexion = new Socket(HOST,PUERTO);
-      while(true){
-      Scanner sc = new Scanner(System.in);
-      comando = new PrintStream(conexion.getOutputStream());
-      comando.println(sc.nextLine());
+        
+        while (true){
+          
+        System.out.println("A ver");
+        Scanner sc = new Scanner(System.in);
+        comando = new PrintStream(conexion.getOutputStream());
+        linea = sc.nextLine();
+        System.out.println(linea);
+        comando.println(linea);
+
+        if (linea.equals("Solicitud libro")){
+          try{
+            System.out.println("Intentando conectar al socket");
+            Thread.sleep(2000);
+            Socket conexion2 = new Socket(HOST, 1234);
+            System.out.println("Entrando en el nuevo socket");
+            InputStream in = conexion2.getInputStream();
+            FileOutputStream out = new FileOutputStream(new File("imagen_copia2.png"));
+
+            byte buf[] = new byte[1024];
+            int len;
+
+            while((len = in.read(buf))!=-1){
+              System.out.println("Empezando a descargar el archivo");
+              out.write(buf,0,len);
+            } 
+            System.out.println("Descarga terminada");
+            in.close();
+
+          }catch(Exception e){
+            System.out.println("Error en la segunda conexion del socket" + e.getMessage());
+          }
+        }
+        
 
 
-
-      buffentrada = new InputStreamReader(conexion.getInputStream());
-      entrada     = new BufferedReader(buffentrada);
-
-      while((line = entrada.readLine()) != null){
-        System.out.println(line);
-      }
-    }
+        buffentrada = new InputStreamReader(conexion.getInputStream());
+        entrada     = new BufferedReader(buffentrada);
+        while((line = entrada.readLine()) != null){
+          System.out.println(line);
+        }
+        }
+      
     }catch(Exception e){
       System.out.println("cancer" +e.getMessage());
 
--- servidor.java
+++ servidor.java
@@ -19,6 +19,10 @@ public class servidor {
     PrintStream respuesta;
     String recibido;
 
+    byte[] byteArray;
+
+    int in;
+
     String menu = " 1) ESTADO_DESCARGAS \n 2) LISTA_LIBROS \n 3) SOLICITUD <libro> \n 4) LIBROS_DESCARGADOSxSERVIDOR  \n";
 
     try {
@@ -26,21 +30,53 @@ public class servidor {
       servidor = new ServerSocket(PUERTO);
       System.out.println("Esperando conexion");
       while(true){
-      conexion = servidor.accept();
-      System.out.println("Conexion establecida");
-      while(true){
+        conexion = servidor.accept();
+        System.out.println("Conexion establecida");
+        
+        while(true){
 
-      buffentrada = new InputStreamReader(conexion.getInputStream());
-      entrada     = new BufferedReader(buffentrada);
-      recibido    = entrada.readLine();
+          respuesta   = new PrintStream(conexion.getOutputStream());
+          buffentrada = new InputStreamReader(conexion.getInputStream());
+          entrada     = new BufferedReader(buffentrada);
 
-      System.out.println(recibido);
+          
+          if ((recibido = entrada.readLine()) != null) {
+            System.out.println(recibido);
+            System.out.println(recibido);
+            if (recibido.equals("Solicitud libro")){
 
-      if (recibido != null) {
-        respuesta   = new PrintStream(conexion.getOutputStream());
-        respuesta.printf(menu);
+              try{
+                ServerSocket servidor2 = new ServerSocket(1234);
+                //while(true){
+                  System.out.println("Esperando conexion en el nuevo socket\n");
+                  Socket conexion2 = servidor2.accept();
+                  System.out.println("Conectado en el nuevo socket\n");
+                  final File archivoLocal = new File("imagen_share.png");
+                  BufferedInputStream entradaArchivo = new BufferedInputStream(new FileInputStream(archivoLocal));
+                  OutputStream pt;
+                  byteArray = new byte[8192];
+                  pt = conexion2.getOutputStream();
+                  while ((in = entradaArchivo.read(byteArray))!=-1){
+                    System.out.printf("Iniciando transferencia de archivo");
+                    respuesta.printf("Iniciando transferencia de archivo");
+                    pt.write(byteArray,0,in);    
+                  }
+                  System.out.println("Termino transferencia");
+                  pt.close();
+                //}
+              }catch(Exception e){
+                  System.out.println("Hubo un error al conectarse al segundo socket "+e.getMessage());
+              }
+            }
+                 
+          }
+
+          
+          respuesta.printf("Se ha establecido correctamente la conexion con el servidor\n");
+          respuesta.printf(menu);
+          
+        
         }
-      }
       }
 
     }catch(Exception e)
@@ -53,5 +89,5 @@ public class servidor {
 
 
 
-
+ 
 }
